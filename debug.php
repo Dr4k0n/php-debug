dcd99068cb87df9fb0ea90ebe43521cf3e651531
Anton Romanov <dr4k0n@list.ru>
2014-06-30 14:25:59 +0400

    omit E_DEPRECATED

--- debug.php
+++ debug.php
@@ -3,7 +3,7 @@
 error_reporting (-1);
 ini_set ('display_errors', 1);
 ini_set ('display_startup_errors', 1);
-set_error_handler (function ($level, $message, $file, $line = 0, $context) { die ("[ERROR] Error: $level, $message, $file:$line\n"); });
+set_error_handler (function ($level, $message, $file, $line = 0, $context) { if ($level != E_DEPRECATED) die ("[ERROR] Error: $level, $message, $file:$line\n"); });
 register_shutdown_function (function () { if ($error = error_get_last()) die ("[SHUTDOWN] Fatal error: {$error['message']}, {$error['file']}:{$error['line']}\n"); });
 set_exception_handler (function ($e) { die ("[EXCEPTION] " . get_class($e) . " {$e->getMessage()}, {$e->getFile()}:{$e->getLine()}\n"); } );
 
